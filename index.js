5eb70fcd60f38be5d3462277737d3cbbc102914d
63Thorne <mpendu2016@gmail.com>
2023-12-16 14:02:13 +0200

    Fiirst commit

---
+++ index.js
@@ -0,0 +1,428 @@
+const button = document.getElementById("btn");
+const audios = document.getElementById("audios");
+
+audios.style.display ="none";
+
+button.addEventListener("click", (event) => {
+    if(audios.style.display == "none"){
+        audios.style.display ="block";
+    } else {
+        audios.style.display ="none";
+    }
+});
+
+
+const button1 = document.getElementById("btn1");
+const audios1 = document.getElementById("audios1");
+
+audios1.style.display ="none";
+
+button1.addEventListener("click", (event) => {
+    if(audios1.style.display == "none"){
+        audios1.style.display ="block";
+    } else {
+        audios1.style.display ="none";
+    }
+});
+
+
+const button2 = document.getElementById("btn2");
+const audios2 = document.getElementById("audios2");
+
+audios2.style.display ="none";
+
+button2.addEventListener("click", (event) => {
+    if(audios2.style.display == "none"){
+        audios2.style.display ="block";
+    } else {
+        audios2.style.display ="none";
+    }
+});
+
+
+const button3 = document.getElementById("btn3");
+const audios3 = document.getElementById("audios3");
+
+audios3.style.display ="none";
+
+button3.addEventListener("click", (event) => {
+    if(audios3.style.display == "none"){
+        audios3.style.display ="block";
+    } else {
+        audios3.style.display ="none";
+    }
+});
+
+const button4 = document.getElementById("btn4");
+const audios4 = document.getElementById("audios4");
+
+audios4.style.display ="none";
+
+button4.addEventListener("click", (event) => {
+    if(audios4.style.display == "none"){
+        audios4.style.display ="block";
+    } else {
+        audios4.style.display ="none";
+    }
+});
+
+
+const button5 = document.getElementById("btn5");
+const audios5 = document.getElementById("audios5");
+
+audios5.style.display ="none";
+
+button5.addEventListener("click", (event) => {
+    if(audios5.style.display == "none"){
+        audios5.style.display ="block";
+    } else {
+        audios5.style.display ="none";
+    }
+});
+
+
+const button6 = document.getElementById("btn6");
+const audios6 = document.getElementById("audios6");
+
+audios6.style.display ="none";
+
+button6.addEventListener("click", (event) => {
+    if(audios6.style.display == "none"){
+        audios6.style.display ="block";
+    } else {
+        audios6.style.display ="none";
+    }
+});
+
+
+const button7 = document.getElementById("btn7");
+const audios7 = document.getElementById("audios7");
+
+audios7.style.display ="none";
+
+button7.addEventListener("click", (event) => {
+    if(audios7.style.display == "none"){
+        audios7.style.display ="block";
+    } else {
+        audios7.style.display ="none";
+    }
+});
+
+
+const button8 = document.getElementById("btn8");
+const audios8 = document.getElementById("audios8");
+
+audios8.style.display ="none";
+
+button8.addEventListener("click", (event) => {
+    if(audios8.style.display == "none"){
+        audios8.style.display ="block";
+    } else {
+        audios8.style.display ="none";
+    }
+});
+
+
+const button9 = document.getElementById("btn9");
+const audios9 = document.getElementById("audios9");
+
+audios9.style.display ="none";
+
+button9.addEventListener("click", (event) => {
+    if(audios9.style.display == "none"){
+        audios9.style.display ="block";
+    } else {
+        audios9.style.display ="none";
+    }
+});
+
+const button10 = document.getElementById("btn10");
+const audios10= document.getElementById("audios10");
+
+audios10.style.display ="none";
+
+button10.addEventListener("click", (event) => {
+    if(audios10.style.display == "none"){
+        audios10.style.display ="block";
+    } else {
+        audios10.style.display ="none";
+    }
+});
+
+
+
+const button11 = document.getElementById("btn11");
+const audios11 = document.getElementById("audios11");
+
+audios11.style.display ="none";
+
+button11.addEventListener("click", (event) => {
+    if(audios11.style.display == "none"){
+        audios11.style.display ="block";
+    } else {
+        audios11.style.display ="none";
+    }
+});
+
+
+const button12 = document.getElementById("btn12");
+const audios12 = document.getElementById("audios12");
+
+audios12.style.display ="none";
+
+button12.addEventListener("click", (event) => {
+    if(audios12.style.display == "none"){
+        audios12.style.display ="block";
+    } else {
+        audios12.style.display ="none";
+    }
+});
+
+
+const button13 = document.getElementById("btn13");
+const audios13 = document.getElementById("audios13");
+
+audios13.style.display ="none";
+
+button13.addEventListener("click", (event) => {
+    if(audios13.style.display == "none"){
+        audios13.style.display ="block";
+    } else {
+        audios13.style.display ="none";
+    }
+});
+
+
+const button14 = document.getElementById("btn14");
+const audios14 = document.getElementById("audios14");
+
+audios14.style.display ="none";
+
+button14.addEventListener("click", (event) => {
+    if(audios14.style.display == "none"){
+        audios14.style.display ="block";
+    } else {
+        audios14.style.display ="none";
+    }
+});
+
+
+const button15 = document.getElementById("btn15");
+const audios15 = document.getElementById("audios15");
+
+audios15.style.display ="none";
+
+button15.addEventListener("click", (event) => {
+    if(audios15.style.display == "none"){
+        audios15.style.display ="block";
+    } else {
+        audios15.style.display ="none";
+    }
+});
+
+
+const button16 = document.getElementById("btn16");
+const audios16 = document.getElementById("audios16");
+
+audios16.style.display ="none";
+
+button16.addEventListener("click", (event) => {
+    if(audios16.style.display == "none"){
+        audios16.style.display ="block";
+    } else {
+        audios16.style.display ="none";
+    }
+});
+
+
+const button17 = document.getElementById("btn17");
+const audios17 = document.getElementById("audios17");
+
+audios17.style.display ="none";
+
+button17.addEventListener("click", (event) => {
+    if(audios17.style.display == "none"){
+        audios17.style.display ="block";
+    } else {
+        audios17.style.display ="none";
+    }
+});
+
+
+const button18 = document.getElementById("btn18");
+const audios18 = document.getElementById("audios18");
+
+audios18.style.display ="none";
+
+button18.addEventListener("click", (event) => {
+    if(audios18.style.display == "none"){
+        audios18.style.display ="block";
+    } else {
+        audios18.style.display ="none";
+    }
+});
+
+
+const button19 = document.getElementById("btn19");
+const audios19 = document.getElementById("audios19");
+
+audios19.style.display ="none";
+
+button19.addEventListener("click", (event) => {
+    if(audios19.style.display == "none"){
+        audios19.style.display ="block";
+    } else {
+        audios19.style.display ="none";
+    }
+});
+
+
+
+const button20 = document.getElementById("btn20");
+const audios20 = document.getElementById("audios20");
+
+audios20.style.display ="none";
+
+button20.addEventListener("click", (event) => {
+    if(audios20.style.display == "none"){
+        audios20.style.display ="block";
+    } else {
+        audios20.style.display ="none";
+    }
+});
+
+
+const button21 = document.getElementById("btn21");
+const audios21 = document.getElementById("audios21");
+
+audios21.style.display ="none";
+
+button21.addEventListener("click", (event) => {
+    if(audios21.style.display == "none"){
+        audios21.style.display ="block";
+    } else {
+        audios21.style.display ="none";
+    }
+});
+
+
+const button22 = document.getElementById("btn22");
+const audios22 = document.getElementById("audios22");
+
+audios22.style.display ="none";
+
+button22.addEventListener("click", (event) => {
+    if(audios22.style.display == "none"){
+        audios22.style.display ="block";
+    } else {
+        audios22.style.display ="none";
+    }
+});
+
+
+const button23 = document.getElementById("btn23");
+const audios23 = document.getElementById("audios23");
+
+audios23.style.display ="none";
+
+button23.addEventListener("click", (event) => {
+    if(audios23.style.display == "none"){
+        audios23.style.display ="block";
+    } else {
+        audios23.style.display ="none";
+    }
+});
+
+
+const button24 = document.getElementById("btn24");
+const audios24 = document.getElementById("audios24");
+
+audios24.style.display ="none";
+
+button24.addEventListener("click", (event) => {
+    if(audios24.style.display == "none"){
+        audios24.style.display ="block";
+    } else {
+        audios24.style.display ="none";
+    }
+});
+
+
+
+ const button25 = document.getElementById("btn25");
+const audios25 = document.getElementById("audios25");
+
+audios25.style.display ="none";
+
+button25.addEventListener("click", (event) => {
+    if(audios25.style.display == "none"){
+        audios25.style.display ="block";
+    } else {
+        audios25.style.display ="none";
+    }
+});
+
+
+const button26 = document.getElementById("btn26");
+const audios26 = document.getElementById("audios26");
+
+audios26.style.display ="none";
+
+button26.addEventListener("click", (event) => {
+    if(audios26.style.display == "none"){
+        audios26.style.display ="block";
+    } else {
+        audios26.style.display ="none";
+    }
+});
+
+
+const button27 = document.getElementById("btn27");
+const audios27 = document.getElementById("audios27");
+
+audios27.style.display ="none";
+
+button27.addEventListener("click", (event) => {
+    if(audios27.style.display == "none"){
+        audios27.style.display ="block";
+    } else {
+        audios27.style.display ="none";
+    }
+});
+
+
+const button28 = document.getElementById("btn28");
+const audios28 = document.getElementById("audios28");
+
+audios28.style.display ="none";
+
+button28.addEventListener("click", (event) => {
+    if(audios28.style.display == "none"){
+        audios28.style.display ="block";
+    } else {
+        audios28.style.display ="none"; 
+    }
+});
+
+
+const button29 = document.getElementById("btn29");
+const audios29 = document.getElementById("audios29");
+
+audios29.style.display ="none";
+
+button29.addEventListener("click", (event) => {
+    if(audios29.style.display == "none"){
+        audios29.style.display ="block";
+    } else {
+        audios29.style.display ="none";
+    }
+});    
+
+
+
+
+
+
+
+
+
